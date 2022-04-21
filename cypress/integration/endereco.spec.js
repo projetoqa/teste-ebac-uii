0cb0cf191f74d0d989a0ba9fc54e737ba32d1e0f
Kátia Veloso <vcursoqa@gmail.com>
2022-04-21 19:34:48 -0300

    Alteração teste automatizado

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,11 +11,30 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
     
 
-   it.only('Deve fazer cadastro de faturamento com sucesso', () => {
-        EnderecoPage.editarEnderecoFaturamento()
+   it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Kátia', 'Veloso', 'sdpata', 'Brasil', 'Av Dois', '5125', 'Contagem', 'Minas Gerais', '32140500', '3132561458', 'katsdf@gmail.com' )
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
 
     });
 
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+
+    });
+
+    
 
 });
